9189b827229165a67f6f8b461e08f649076fc010
Biki <102693150+Bikash1075@users.noreply.github.com>
2023-01-17 10:06:05 +0530

    Delete count_number_of_words.py

--- count_number_of_words.py
+++
@@ -1,9 +0,0 @@
-
-import pandas as pd
-reader = pd.read_csv('my_data.txt')
-print(reader.head())
-
-reader["Count_Of_Words1"] = reader['Details'].apply(lambda n : len(n.split()))
-print(reader.head())
-
-
